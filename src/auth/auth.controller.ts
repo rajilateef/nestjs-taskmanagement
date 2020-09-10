de870f691872ab2688742a63388e5f3247d63e34
rajilateef <rajilateef.rl@gmail.com>
2020-09-10 22:38:09 +0100

    updates

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -9,7 +9,7 @@ export class AuthController {
   ) {}
 
   @Post('/signup')
-  signUp(@Body() authCredentialsDto:AuthCredentialsDto) {
+  signUp(@Body(ValidationPipe) authCredentialsDto:AuthCredentialsDto) {
     return this.authService.signUp(authCredentialsDto);
   }
 }
